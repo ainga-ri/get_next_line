358a1e8a74b35fef4c96864cc3bf245999460974
msindreu <msindreuriba@gmail.com>
2022-07-14 18:59:47 +0200

    new_guardado

--- get_next_line.c
+++ get_next_line.c
@@ -6,43 +6,58 @@
 /*   By: msindreu <msindreu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 16:14:58 by msindreu          #+#    #+#             */
-/*   Updated: 2022/07/14 14:05:45 by ainga-ri         ###   ########.fr       */
+/*   Updated: 2022/07/14 18:11:33 by msindreu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_get_line(char *str)
+{
+	char	*line;
+	int 	i;
+
+	i = 0;
+	while (fstr[i] == '\n')
+	{
+		i++;
+	}
+	line = ft_substr(str, 0, i);
+	if (!line)
+		return (NULL);
+	return (line);
+}
+
 char	*get_next_line(int fd)
 {
 	
-	static char	*guardado;
+	char	*guardado;
+	static char	*new_guardado;
 	int		i;
 	char	*line;
 	
 	line = "";
-	guardado = (char *)malloc(BUFFER_SIZE *  sizeof(char));
+	i = 1;
+	guardado = (char *)calloc((BUFFER_SIZE + 1) *  sizeof(char));
 	if (!guardado)
 		return (NULL);
-	i = read(fd, guardado, BUFFER_SIZE);
-	if (i == -1)
+	while (!ft_strchr(guardado, '\n') && i > 0)
 	{
-		if (guardado)
-			free(guardado);
-		return (NULL);
-	}
-	while (!ft_strchr(guardado, '\n'))
-	{	
-		line = ft_strjoin(line, guardado);
-		if (!line)
-			return (NULL);
 		i = read(fd, guardado, BUFFER_SIZE);
 		if (i == -1)
+		{
+			if (guardado)
+				free(guardado);
 			return (NULL);
-		printf("%s\n", line);
-
+		}
+		new_guardado = ft_strjoin(new_guardado, guardado);
+		if (!new_guardado)
+			return (NULL);
 	}
-
+	//compiamos linea en line
+	line = ft_get_line(new_guardado);
+	//borramos line en new_guardado
+	new_guardado = ft_clean(new_guardado);
 	printf("Mida del retorn del read: %d\n", i);
-
 	return (line);
 }
